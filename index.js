ad35221d486c46cf4b172a2eebc287d5882f0e01
Juan <juan@seasonmint.com>
2019-11-19 18:20:21 -0500

    this is the final working version with no styling

--- index.js
+++ index.js
@@ -11,9 +11,17 @@ app.get("/", function(req,res){
 });
 // the following makes a request to an extrenal server (api) by using the request module from npm
 app.post("/", function(req,res){
-    request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response, body){
-        console.log(body)
 
+    var crypto = req.body.crypto;// storing the bodys crypto value from index
+    var fiat = req.body.Fiat;//storing the bodys fiat value from index
+    var baseUrl = "https://apiv2.bitcoinaverage.com/indices/global/ticker/"; //stores part of the url in a variable
+    var finalURL = baseUrl + crypto + fiat;// combines the base url with the crypto and fiat variables that hold the index files body values
+    request(finalURL, function(error, response, body){//final url in the parameters is the endpoint adress
+        var data = JSON.parse(body);
+        var price = data.last;
+        res.send("<h1>The current price of " + crypto+ " is " + price+ fiat + "</h1>")
+        //console.log(body) // the body of whatever is located at the above endpoint
+//JSON.stringify(wardrobe) converts a javascript object into JSON format while parse brings it back to workable javascript object
     })
 
 });
